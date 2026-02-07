9ae390a9d5497f97c00bb51250620227d16e7afe
hypensterisk <roykhanalaushaf0@gmail.com>
2026-02-07 21:27:40 +0000

    feat: add file reading functionality with commander dependency

--- bin/reside.js
+++ bin/reside.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+import { readFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { cwd, exit } from 'node:process'
+
+import { program } from 'commander'
+
+program.argument('<source>').action(async (inputPath) => {
+  try {
+    const sourcePath = resolve(cwd(), inputPath)
+    const sourceCode = await readFile(sourcePath, { encoding: 'utf8' })
+  } catch (error) {
+    if (error?.code === 'ENOENT') {
+      console.error(`Error: Cannot find source '${error.path}'`)
+      exit(1)
+    } else throw error
+  }
+}).parse()
